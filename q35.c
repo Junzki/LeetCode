a4c3449268937ab8fcd25e456d500c873103c82e
Andrew Junzki <andrew.junzki@gmail.com>
2018-03-06 23:30:02 +0800

    Add solution of problem #35. [EASY]

---
+++ q35.c
@@ -0,0 +1,22 @@
+/*
+ * Problem #35
+ * https://leetcode.com/problems/search-insert-position/
+ */
+
+int searchInsert(int* nums, int numsSize, int target) {
+    int index;
+    
+    for (index = 0; index < (numsSize - 1); ++index)
+    {
+        if (target <= nums[index])
+            return index;
+        
+        else if (target > nums[index] && target <= nums[index + 1])
+            return index + 1;
+    }
+    
+    if (target <= nums[index])
+        return index;
+    
+    return numsSize;
+}
